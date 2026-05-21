99c50ae6c110ab88c55a1b07f28c9d709c45804c
Anthony-Hannoteaux <tony.hannoteaux@gmail.com>
2026-05-21 10:34:14 +0200

    backend: add endpoint locations and test to get all sensors

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,13 +1,17 @@
 import express from "express";
 import healthRouter from "./healthRoutes.js"
 import locationRouter from "./locationRoutes.js";
+import sensorRouter from "./sensorRoutes.js";
 
 const router = express.Router();
 
 // Vérification de l'état de l'API et de la connexion avec la base de données PostgreSQL
-router.use('/health', healthRouter)
+router.use('/health', healthRouter);
 
 // Endpoint communiquant avec la table "location"
-router.use('/locations', locationRouter)
+router.use('/locations', locationRouter);
+
+// Endpoint communiquant avec la table "sensor"
+router.use('/sensors', sensorRouter);
 
 export default router;
