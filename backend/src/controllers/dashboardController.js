6a98270724963c100a4c9d98d004d7d9a02077e6
Anthony-Hannoteaux <tony.hannoteaux@gmail.com>
2026-05-25 20:07:19 +0200

    backend(endpoint): add dashboard endpoint to get alerts

--- backend/src/controllers/dashboardController.js
+++ backend/src/controllers/dashboardController.js
@@ -58,6 +58,23 @@ const dashboardControler = {
                 error: error.message
             })
         }
+    },
+
+    // Route GET /api/dashboard/alerts
+    getDashboardAlerts: async (req, res) => {
+        try {
+            const result = await Alert.findAll();
+            return res.status(200).json({
+                status: "Succès",
+                data: result
+            })
+        } catch (error) {
+            return res.status(500).json({
+                status: "Echec",
+                message: "Erreur survenue lors de la récupération des alertes du dashboard",
+                error: error.message
+            })
+        }
     }
 }
 
--- backend/src/routes/dashboardRouters.js
+++ backend/src/routes/dashboardRouters.js
@@ -5,6 +5,6 @@ const dashboardRouter = express.Router();
 
 dashboardRouter.get("/locations", dashboardControler.getDashboardLocation);
 dashboardRouter.get("/sensor/:sensorId/measures", dashboardControler.getDashboardSensorMeasures);
-
+dashboardRouter.get("/alerts", dashboardControler.getDashboardAlerts);
 
 export default dashboardRouter;
