fa2c38b4c39ae7c997b21b6185c833f4cc9c2c3c
Anthony-Hannoteaux <tony.hannoteaux@gmail.com>
2026-05-26 22:00:38 +0200

    backend: minimum config CORS to autorize frontend origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,14 @@
 import express from "express";
+import cors from "cors";
 import router from "./routes/routes.js";
 
 const app = express();
+
+const corsOptions = {
+    origin: process.env.FRONTEND_ORIGIN || "http://localhos:5173"
+}
+
+app.use(cors(corsOptions))
 
 app.use(express.json());
 
