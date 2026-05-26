135bfeaa7b9775265861541e2c73ee13e05c6c52
Anthony-Hannoteaux <tony.hannoteaux@gmail.com>
2026-05-26 20:42:25 +0200

    frontend(stylization): Initialize SCSS variables and tests boxicons's package on HomePage render

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import AppRouter from "./components/AppRouter"
-import './styles/index.scss'
 
 export default function App() {
 	return <AppRouter />
--- frontend/src/pages/HomePage/index.jsx
+++ frontend/src/pages/HomePage/index.jsx
@@ -1,7 +1,9 @@
+import { Home } from '@boxicons/react';
+
 export default function HomePage() {
     return (
         <main>
-            <h1>HomePage - Dashboard Supervision IoT</h1>
+            <h1><Home /> HomePage - Dashboard Supervision IoT</h1>
             <p>Test de rendu et test des routes avec ReactRouter</p>
         </main>
     )
