2a863f12cbd7dff5ae7317487c8fc07f04d1fc66
Anthony-Hannoteaux <tony.hannoteaux@gmail.com>
2026-05-30 18:12:48 +0200

    frontend(feat): add Card component

---
+++ frontend/src/components/ui/Card/index.jsx
@@ -0,0 +1,26 @@
+import './styles.scss'
+
+export default function Card({ title, measureTemp, measureHum }) {
+    return (
+        <article className='dashboard-card'>
+            <header className='dashboard-card__header'>
+                <h3 className='dashboard-card__title'>
+                    {title}
+                </h3>
+            </header>
+            <div className='dashboard-card__body'>
+                <div className='dashboard-card__measure__wrapper'>
+                    <span className='measure-type'>Température</span>
+                    <span className='measure-value'>{measureTemp}</span>
+                </div>
+                <div className='dashboard-card__measure__wrapper'>
+                    <span className='measure-type'>Humidité</span>
+                    <span className='measure-value'>{measureHum}</span>
+                </div>
+            </div>
+            <div>
+                <h4>Statistiques</h4>
+            </div>
+        </article>
+    )
+}
